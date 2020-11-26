15fc2aff9d021ebedf7727d5180e699d9fb4e403
mxxjing <949672675@qq.com>
2020-11-26 19:32:24 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,20 @@
-    
-    var baseURL = 'http://ajax.frontend.itheima.net'
-    $.ajaxPrefilter(function(option){
-        option.url = baseURL + option.url
-    })
+
+var baseURL = 'http://ajax.frontend.itheima.net'
+$.ajaxPrefilter(function (option) {
+    option.url = baseURL + option.url
+
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 拦截所有响应，判断身份认证信息
+    option.complete = function (res) {
+        console.log(res);
+        if(res.responseJSON.status !== 0 && res.responseJSON.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = "/login.html"
+        }
+    }
+})
---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+$(function () {
+
+    getUserInfo()
+
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 清除token
+            localStorage.removeItem('token')
+            // 跳转到登陆页面
+            location.href = '/login.html'
+            // 关闭询问框
+            layer.close(index);
+        });
+    })
+
+})
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            renderavatar(res.data)
+        }
+    })
+}
+
+function renderavatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html("欢迎&nbsp;" + name)
+    if (user.user_pic) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+        $('.layui-nav-img').hide()
+    }
+}
+
